571ee13e909ee3471db27eac936bffc326157c3e
Gregory Ashton <gash789@gmail.com>
2016-05-05 15:24:15 +0100

    Half-read through of C9
    1) Remove distribution of jumps section: not relevant
    2) Move generic RW to appendix to make a cleaner distinction
    3) Have not yet read though the 2-state switching or generic RW

--- analytic_timing_noise_cgw/img/IllustrationPlots.py
+++ analytic_timing_noise_cgw/img/IllustrationPlots.py
@@ -16,29 +16,31 @@ dt = time[1] - time[0]
 kappa = 0.0
 #F1hat = np.random.normal(0, 0.5, N); print F1hat
 F1hat = [-0.06673121, -0.16184903, 0.07196745, 0.29913356,
-            0.43034329, -0.3241269,  -0.48430681, -0.1279246, 0.0132123]
+          0.43034329, -0.3241269,  -0.48430681, -0.1279246, 0.0132123]
 
 # Add the 0 line and f1dot
 ax1.axhline(0, color="k")
 DeltaF1 = np.cumsum(F1hat)
-ax1.step(time, DeltaF1, lw=2, color="b", where="post")
+p0, = ax1.step(time, DeltaF1, lw=2, color="k", where="post",
+               )
 
 # Add the reference times
-for i in range(len(time)):
-    ax1.plot([time[i]+kappa*dt, time[i]+kappa*dt,], [0, DeltaF1[i]], 
-            ls="--", color="k", lw=0.8)
-p1, = ax1.plot(time[:-1]+kappa*dt, DeltaF1[:-1], "ro")
-
+#for i in range(len(time)):
+#    ax1.plot([time[i]+kappa*dt, time[i]+kappa*dt,], [0, DeltaF1[i]],
+#            ls="--", color="k", lw=0.8)
+#p1, = ax1.plot(time[:-1]+kappa*dt, DeltaF1[:-1], "ro")
+#
+#
 
 # Fill up integration
 def custom_fill(ax, x, y, dx):
-    ax.fill_between([x, x+dx], 
+    ax.fill_between([x, x+dx],
                      y1=0, y2=y,
                      alpha=.5, color="g")
 
 end = N-2
 for i in range(end):
-    custom_fill(ax1, time[i], DeltaF1[i], dt) 
+    custom_fill(ax1, time[i], DeltaF1[i], dt)
 
 custom_fill(ax1, time[end], DeltaF1[end], kappa*dt)
 
@@ -61,9 +63,9 @@ plt.text(1.5, sign*text_off, "$\Delta T$", horizontalalignment='center')
              #arrowprops = dict(arrowstyle='<->'))
 #plt.text(end+.5*kappa*dt, sign*text_off, "$\kappa \Delta T$", horizontalalignment='center')
 
-# Frequency 
+# Frequency
 #ax2 = ax1.twinx()
-#DeltaF0 = dt * np.array([np.sum(DeltaF1[:i]) + .5 * DeltaF1[i] 
+#DeltaF0 = dt * np.array([np.sum(DeltaF1[:i]) + .5 * DeltaF1[i]
 #                                      for i in range(N)])
 #p2, = ax2.step(time, DeltaF0, where="post",
 #         color="g", lw=2)
@@ -77,18 +79,28 @@ ax1_ymax = 1.1*max(abs(DeltaF1.min()), abs(DeltaF1.max()))
 ax1.set_yticks(np.linspace(-ax1_ymax, ax1_ymax, 3))
 
 ax1.set_xlim(0, N-1.1)
-ax1.set_xlabel("Time")
-ax1.set_xticks([end+kappa*dt])
-ax1.set_xticklabels(["$t_{i}$"])
+ax1.set_xlabel("time")
 
-ax1.set_yticklabels([])
-ax1.set_ylabel("$\Delta\dot{f}$", rotation="horizontal", 
-              labelpad=15.0)
+n = len(time)-2
+reftimes = [time[i]+kappa*dt for i in range(n)]
+reftimes.append(end+kappa*dt)
+xticklabels = [r"$t_\mathrm{{ref}}^{{i-{}}}$".format(n-i) for i in range(n)]
+xticklabels.append("$t_\mathrm{ref}^i = t^i$")
+ax1.tick_params(direction='in', pad=10)
+ax1.set_xticks(reftimes)
+ax1.set_xticklabels(xticklabels)
+ax1.set_yticks([0])
+ax1.set_yticklabels(["0"])
+
+ax1.set_ylabel("spin-down rate", rotation="vertical",
+              labelpad=18.0)
 r = Rectangle((0, 0), 1, 1, color="g", alpha=.5) # creates rectangle patch for legend use.
-plt.legend([p1, r], [r"$t_{\textrm{ref}}$", 
-                     r"$\Delta f(t)= \int \Delta\dot{f}(t)dt$",
-                     #r"$\Delta f_{i}$"
-                     ],
+plt.legend([p0, r],
+           [r"$\Delta\dot{f}$",
+            #r"$t_{\textrm{ref}}$",
+            r"$\Delta f(t)= \int \Delta\dot{f}(t)dt$",
+            #r"$\Delta f_{i}$"
+            ],
             loc=2, frameon=False)
+plt.tight_layout()
 plt.savefig("Illustration_F1_int.pdf")
-plt.show()
