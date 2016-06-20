a312629646f262d0c46a4f7b6dd5f9935f427634
Gregory Ashton <gash789@gmail.com>
2016-06-20 14:54:38 +0100

    Updates to C2 following Wynn and Grahams comments

--- timing_variations/img/StochasticResonanceExample.py
+++ timing_variations/img/StochasticResonanceExample.py
@@ -12,22 +12,26 @@ def eom(x, t, Lambda=1, epsilon=0.001, D=1.5, omega0=1e2):
     return Lambda * x - pow(x, 3) + noise + epsilon * np.cos(omega0 * t)
 
 dt = 0.1
-time = np.arange(0, 1000, dt)
+time = np.arange(0, 500, dt)
 
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, sharex=True, figsize=(3.5, 5))
+fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, sharey=True, figsize=(6, 2))
 
 for ax, (j, D) in zip([ax1, ax2, ax3], enumerate([0.1, 1.5, 6.6])):
     x = [1.0]
     for i, t in enumerate(time[:-1]):
         dx = eom(x[i], t, D=D)
         x.append(x[i]+dx*dt)
-    ax.plot(time, x)
+    ax.plot(time, x, color='k')
     ax.set_ylim(-2, 2)
     ax.yaxis.set_major_locator(ticker.MaxNLocator(4))
     ax.set_xticklabels([])
-    ax.set_ylabel("$\sigma$={}".format(D), rotation="horizontal", labelpad=15)
+    ax.set_title("$\sigma$={}".format(D))
 
+ax1.set_xlabel("time")
+ax2.set_xlabel("time")
 ax3.set_xlabel("time")
+
+ax1.set_ylabel('$x$-position of particle')
 
 plt.tight_layout()
 plt.savefig("Stochastic_resonance.png")
